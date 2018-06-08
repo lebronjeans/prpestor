01f4a3d83db923725728136dc9f1bc435ca5c755
cherin <cherin@localhost.localdomain>
2018-06-07 23:36:37 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.write("<script  src='unicosnio.js'></script>");
+
+
+//var head= document.getElementsByTagName('head')[0];
+   //var script= document.createElement('script');
+   //script.type= 'text/javascript';
+   //script.src= 'helper.js';
+   //head.appendChild(script); 
+
+
+
+
+//function import(url){
+ // var script = document.createElement('script');
+ // var parentScript = document.getElementById('myscript');
+ // parentScript = parentScript.src;
+ // parentScript = parentScript.split('/');
+ // parentScript.pop();
+ // parentScript = parentScript.join();
+ // parentScript = parentScript.replace(/,/gi, '/');
+ // script.src = parentScript+url;
+ // var body = document.body;
+ // body.appendChild(script);
+//}
+
+
+//import('js/unicosnio.js');
